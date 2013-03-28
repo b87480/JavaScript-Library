8025ab2e454d98bdbdbadc493202b1aafc01704f
Joshua Whitney <jwhitney114@fullsail.edu>
2013-03-27 22:32:36 -0400

    Check Email Function

--- Whitney_Joshua.js
+++ Whitney_Joshua.js
@@ -8,3 +8,60 @@
 
 //Start Library
 
+// String Functions
+
+
+//Phone Number Function
+
+//Email Function
+function checkEmail(email) {
+	
+	var x = email;
+	var a = x.indexOf("@");
+	var d = x.lastIndexOf(".");
+	
+		if (a < 1 || d < a + 2 || d + 2 >= x.length) {
+  			return false;
+  			}
+};
+checkEmail("jwhitney114@gmail");
+
+
+
+//URL Function
+
+
+//UpperCase Function
+
+
+//Change String Function
+
+
+// Number Functions
+
+
+//Format a Number Function
+
+
+//Fuzzy-Match Number Function
+
+
+//Dates Number Function
+
+
+//Return Number Function
+
+
+// Array Functions
+
+
+//Smallest Value Array Function
+
+
+//Total Value Function
+
+
+//Array Sorter Function
+
+
+//End Library
